a44d537d7fc029598c7e7f8fd3b15d9338e86f15
Hriday-paul <hridoychandrapaul.10@gmail.com>
2025-06-12 15:53:13 +0600

    fix login page responsive issue

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,12 +4,12 @@ import authImg from '@/assets/image/authBg.png'
 
 const layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className='flex flex-row gap-5 items-center max-h-screen'>
-            <div className='w-full lg:w-1/2'>
+        <div className='flex flex-col lg:flex-row gap-5 items-center max-h-screen'>
+            <div className='hidden lg:block w-full lg:w-1/2'>
                 <Image
                     src={authImg} placeholder='blur' alt="auth bf image" priority className="h-screen w-full object-cover relative" />
             </div>
-            <div className='w-full lg:w-1/2 flex justify-center items-center'>
+            <div className='h-screen lg:h-auto w-full lg:w-1/2 flex justify-center items-center'>
                 {children}
             </div>
         </div>
